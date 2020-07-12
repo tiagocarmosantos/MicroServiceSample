284a0de34048d98bd22a3c924ef67a4b0a335b43
Tiago Carmo Santos <tiagocarmosantos@hotmail.com>
2020-07-12 00:14:31 -0300

    add new feature to picme API - Filter by Objects Feature

--- api/picMe/picMeService.js
+++ api/picMe/picMeService.js
@@ -15,6 +15,46 @@ PicMe.route('count', function(req, res, next) {
 })
 
 PicMe.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+PicMe.after('get', filterRoute)
+
+function filterRoute(req, res, next) {
+	if(!!req.query.filter) {
+		PicMe.find(function(error, value){
+			if(error){
+				res.status(500).json({errors: [error]})
+			} else {
+				let filterParam = req.query.filter.split(':')[0]
+				let filterValue = req.query.filter.split(':')[1]
+				valueFiltered = value.filter((item) => {
+					let itemValue = getPropByString(item, filterParam)
+					return itemValue == filterValue
+				})
+				res.json(valueFiltered)
+			}
+		})
+	} else{
+		next()
+	}
+}
+
+function getPropByString(obj, propString) {
+    if (!propString)
+        return obj;
+
+    var prop, props = propString.split('.');
+
+    for (var i = 0, iLen = props.length - 1; i < iLen; i++) {
+        prop = props[i];
+
+        var candidate = obj[prop];
+        if (candidate !== undefined) {
+            obj = candidate;
+        } else {
+            break;
+        }
+    }
+    return obj[props[i]];
+}
 
 function sendErrorsOrNext(req, res, next) {
 	const bundle = res.locals.bundle
